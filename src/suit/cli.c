5b6a0f777b55efbb90c6155ca2a3459a5a08a41f
Samuel Lindemer <samuel.lindemer@gmail.com>
2020-06-11 16:37:46 +0200

    Manifest unwrapper needs fixing

--- src/suit/cli.c
+++ src/suit/cli.c
@@ -30,18 +30,18 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **/
 
-#define USAGE fprintf(stderr,                                                                   \
-        "Copyright (c) 2020, RISE Research Institutes of Sweden\n"                              \
-        "All rights reserved.\n"                                                                \
-        "\nUsage: %s [-hknpu] > [output file] < [input file]\n"                                 \
-        "\t-h displays this text\n"                                                             \
-        "\t-k [key file]\n"                                                                     \
-        "\t-n [sequence number]\n"                                                              \
-        "\t-p parses a manifest from stdin and decodes it\n"                                    \
-        "\t-u [remote firmware URI]\n"                                                          \
-        "\nExamples:\n"                                                                         \
-        "%s -k keys/priv.pem -n 0 -u coaps://[::1]/firmware > manifest.cbor < firmware.exe\n"   \
-        "%s -k keys/pub.pem -p < manifest.cbor\n"                                               \
+#define USAGE fprintf(stderr,                                                           \
+"Copyright (c) 2020, RISE Research Institutes of Sweden\n"                              \
+"All rights reserved.\n"                                                                \
+"\nUsage: %s [-hknpu] > [output file] < [input file]\n"                                 \
+"\t-h displays this text\n"                                                             \
+"\t-k [key file]\n"                                                                     \
+"\t-n [sequence number]\n"                                                              \
+"\t-p parses a manifest from stdin and decodes it\n"                                    \
+"\t-u [remote firmware URI]\n"                                                          \
+"\nExamples:\n"                                                                         \
+"%s -k keys/priv.pem -n 0 -u coaps://[::1]/firmware > manifest.cbor < firmware.exe\n"   \
+"%s -k keys/pub.pem -p < manifest.cbor\n"                                               \
 , argv[0], argv[0], argv[0]);
 
 #define SUIT_CLASS_ID   "1492af1425695e48bf429b2d51f2ab45"
@@ -59,7 +59,6 @@
 
 void read_stdin(uint8_t * wptr, uint32_t * bytes);
 void hash_firmware(const uint8_t * buffer, const uint32_t len_buffer, suit_component_t * component); 
-void read_manifest(const uint8_t * buffer, const size_t len_buffer, const char * pem);
 void xxd(const uint8_t * data, size_t len, int w);
 
 int main (int argc, char *argv[])
@@ -97,12 +96,23 @@ int main (int argc, char *argv[])
 
     /* buffer for I/O */
     uint32_t ibytes = 0;
+    size_t obytes = HEAP_BUFFER;
     uint8_t * buffer = (uint8_t *) malloc(HEAP_BUFFER);
     read_stdin(buffer, &ibytes);
     
-    if (p) { /* read_manifest existing manifest from stdin */
+    if (p) { /* parse and decode existing manifest */
 
-        /* do something... */
+        uint8_t * manifest = malloc(HEAP_BUFFER);
+        if (suit_unwrap(pem, buffer, HEAP_BUFFER, (const uint8_t **) &manifest, &obytes)) {
+            fprintf(stderr, "Failed to authenticate manifest.\n");
+            exit(EXIT_FAILURE);
+        }
+    
+        suit_context_t ctx;
+        if (suit_parse(&ctx, manifest, obytes)) {
+            fprintf(stderr, "Failed to parse manifest.\n");
+            exit(EXIT_FAILURE);
+        }
 
     } else { /* write new manifest */
 
@@ -130,17 +140,19 @@ int main (int argc, char *argv[])
         hash_firmware(buffer, ibytes, &ctx.components[0]);
         
         /* encode the manifest */
-        size_t obytes = HEAP_BUFFER;
         suit_encode(&ctx, buffer, &obytes);
 
         /* sign the manifest */
         size_t len_wrapped = HEAP_BUFFER;
         uint8_t * wrapped = (uint8_t *) malloc(HEAP_BUFFER);
-        int err = suit_wrap(pem, buffer, obytes, wrapped, &len_wrapped);
+        if (suit_wrap(pem, buffer, obytes, wrapped, &len_wrapped)) {
+            fprintf(stderr, "Failed to encode manifest.\n");
+            exit(EXIT_FAILURE);
+        }
 
         /* write to stdout */
         fwrite(wrapped, sizeof(uint8_t), len_wrapped, stdout);
-
+        free(wrapped);
     }
 
     free(pem);
@@ -165,20 +177,6 @@ void hash_firmware(const uint8_t * buffer, const uint32_t len_buffer, suit_compo
     component->len_digest = mbedtls_md_get_size(md_info); 
     component->digest_alg = suit_digest_alg_sha256;
     component->size = len_buffer;
-}
-
-void read_manifest(const uint8_t * buffer, size_t len_buffer, const char * pem)
-{
-    size_t bytes = 0;
-    uint8_t * manifest = malloc(HEAP_BUFFER);
-
-    int err;
-    err = suit_unwrap(pem, buffer, len_buffer, (const uint8_t **) &manifest, &bytes);
-    if (err) fprintf(stderr, "suit_unwrap returned: %d\n", err);
-    
-    suit_context_t ctx;
-    err = suit_parse(&ctx, manifest, bytes);
-    if (err) fprintf(stderr, "suit_parse_init returned %d\n", err);
 }
 
 void xxd(const uint8_t * data, size_t len, int w)
