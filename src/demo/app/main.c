d99c08d07d710033897321e20f1bab37457b90e0
Samuel Lindemer <samuel.lindemer@gmail.com>
2020-11-10 10:10:53 +0100

    Disable RPLIDAR in main demo code

--- src/demo/app/main.c
+++ src/demo/app/main.c
@@ -67,7 +67,7 @@
 #include <openthread/platform/alarm-milli.h>
 
 #define ENABLE_COAPS_DFU 
-#define ENABLE_RPLIDAR
+//#define ENABLE_RPLIDAR
 
 // Thingy:91 GPIO pins
 #define SPARE1 NRF_GPIO_PIN_MAP(0, 6)
@@ -288,15 +288,12 @@ int main(int argc, char *argv[])
     nrf_gpio_pin_set(SPARE2);
     nrf_delay_ms(100);
 
-    rplidar_point_t point;
 #endif // ENABLE_RPLIDAR
 
     while (true)
     {
         thread_process();
         app_sched_execute();
-
-        rplidar_get_point(&point);
 
         if (NRF_LOG_PROCESS() == false)
         {
